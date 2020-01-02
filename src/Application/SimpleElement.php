da76338ed6a7b4731b3eff9aab47abbf3d834a44
muslax <muslax@gmail.com>
2020-01-03 02:48:45 +0700

    Create SimpleElement.php

---
+++ src/Application/SimpleElement.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the Gaia Behavioral package.
+ *
+ * @author Arif Muslax <muslax@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gaia\Behavioral\Application;
+
+/**
+ * SimpleElement Class.
+ *
+ * @author Arif Muslax <muslax@gmail.com>
+ */
+class SimpleElement extends Element
+{
+} // END class SimpleElement extends Element
