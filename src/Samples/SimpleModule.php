913d728831707d76ee247c30c5963da507a82574
muslax <muslax@gmail.com>
2020-01-08 22:15:42 +0700

    Update SimpleModule.php

--- src/Samples/SimpleModule.php
+++ src/Samples/SimpleModule.php
@@ -7,27 +7,27 @@ use Gaia\Behavioral\Evidence;
 
 class SimpleModule extends Module    
 {
-    public function __construct($book = null)
-    {
-        parent::__construct($book);
-        // if ($book instanceof Workbook) {
-        //     $this->book = $book;
-        //     $this->init_books();
-        // }
-    }
+    // public function __construct($book = null)
+    // {
+    //     parent::__construct($book);
+    //     // if ($book instanceof Workbook) {
+    //     //     $this->book = $book;
+    //     //     $this->init_books();
+    //     // }
+    // }
     
     public function score(Evidence $evidence) : array
     {
         return ['SCORE' => 'TEST'];
     }
     
-    public function generateWorkbook(Evidence $evidence, array $options = []) : SimpleWorkbook
+    public function generateWorkbook(Evidence $evidence, array $options = []) : ?object
     {
-        return $this->book;
+        return null;
     }
     
-    public function generateReport(Evidence $evidence, array $options = []) : object
+    public function generateReport(Evidence $evidence, array $options = []) : ?object
     {
-        return $this->book;
+        return null;
     }
 }
